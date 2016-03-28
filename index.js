c8c119d757df4e34ecb0f8be54d81fbd607af1e7
Eric Lathrop <eric@ericlathrop.com>
2016-03-27 22:25:49 -0400

    Tabs to spaces

--- index.js
+++ index.js
@@ -5,36 +5,36 @@ var colors = [ "#ff0000", "#00ff00", "#0000ff" ];
 var color = 0;
 
 var keys = {
-	37: "left",
-	38: "up",
-	39: "right",
-	40: "down"
+  37: "left",
+  38: "up",
+  39: "right",
+  40: "down"
 };
 var pressed = {};
 
 window.addEventListener("keydown", function(e) {
-	pressed[keys[e.keyCode]] = true;
+  pressed[keys[e.keyCode]] = true;
 });
 window.addEventListener("keyup", function(e) {
-	pressed[keys[e.keyCode]] = false;
+  pressed[keys[e.keyCode]] = false;
 });
 
 var playerX = 50;
 var playerSpeed = 5;
 var render = function(elapsed) {
-	if (pressed["left"]) {
-		playerX -= playerSpeed;
-	}
-	if (pressed["right"]) {
-		playerX += playerSpeed;
-	}
+  if (pressed["left"]) {
+    playerX -= playerSpeed;
+  }
+  if (pressed["right"]) {
+    playerX += playerSpeed;
+  }
 
-	context.fillStyle = colors[color];
-	color++;
-	if (color >= colors.length) {
-		color = 0;
-	}
-	context.fillRect(playerX, 500, 50, 50);
-	window.requestAnimationFrame(render);
+  context.fillStyle = colors[color];
+  color++;
+  if (color >= colors.length) {
+    color = 0;
+  }
+  context.fillRect(playerX, 500, 50, 50);
+  window.requestAnimationFrame(render);
 }
 window.requestAnimationFrame(render);
