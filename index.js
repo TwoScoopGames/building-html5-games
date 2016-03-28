55c63196dce6be5af64c9fad57182cf76b0a0d47
Eric Lathrop <eric@ericlathrop.com>
2016-03-27 22:05:00 -0400

    Add render loop.

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
 
-context.fillStyle = "#ff0000";
-context.fillRect(50, 50, 50, 50);
+var colors = [ "#ff0000", "#00ff00", "#0000ff" ];
+var color = 0;
+
+var render = function(elapsed) {
+	context.fillStyle = colors[color];
+	color++;
+	if (color >= colors.length) {
+		color = 0;
+	}
+	context.fillRect(50, 50, 50, 50);
+	window.requestAnimationFrame(render);
+}
+window.requestAnimationFrame(render);
