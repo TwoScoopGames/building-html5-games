c6aea53d8b39e70c969edeed9ac69486810b1cb3
Eric Lathrop <eric@ericlathrop.com>
2016-03-27 22:25:17 -0400

    Tabs to spaces

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ var colors = [ "#ff0000", "#00ff00", "#0000ff" ];
 var color = 0;
 
 var render = function(elapsed) {
-	context.fillStyle = colors[color];
-	color++;
-	if (color >= colors.length) {
-		color = 0;
-	}
-	context.fillRect(50, 50, 50, 50);
-	window.requestAnimationFrame(render);
+  context.fillStyle = colors[color];
+  color++;
+  if (color >= colors.length) {
+    color = 0;
+  }
+  context.fillRect(50, 50, 50, 50);
+  window.requestAnimationFrame(render);
 }
 window.requestAnimationFrame(render);
