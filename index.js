bcaf34498b3e04ddcca979aacc78f2fc8e11ebff
Eric Lathrop <eric@ericlathrop.com>
2016-03-28 11:19:48 -0400

    Fix overlaps

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ var lastFrameTime = null;
 
 function overlaps(x1, y1, w1, h1, x2, y2, w2, h2) {
   return x1 + w1 > x2 && x1 < x2 + w2 &&
-    y1 + h1 > y1 && y1 < y2 + h2;
+    y1 + h1 > y2 && y1 < y2 + h2;
 }
 
 var render = function(time) {
